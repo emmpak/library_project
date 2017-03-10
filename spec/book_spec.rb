bb91b30ef0ae943d0f4d823bc2dcd081633d2eba
emmpak <eiliev05@yahoo.com>
2017-03-10 16:24:59 +0000

    Library project completed

---
+++ spec/book_spec.rb
@@ -0,0 +1,33 @@
+require 'book'
+
+describe Book do
+
+  before :each do
+    @book = Book.new "Title", "Author", :category
+  end
+
+  describe "#new" do
+    it "returns a new book object" do
+      expect(@book).to be_an_instance_of Book
+    end
+    it "throws an ArgumenError when given fewer than 3 parameters" do
+      expect{Book.new("Title", "Author")}.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#title" do
+    it "returns the correct title" do
+      @book.title.should eq "Title"
+    end
+  end
+  describe "#author" do
+    it "returns the correct title" do
+      @book.author.should == "Author"
+    end
+  end
+  describe "#category" do
+    it "returns the correct title" do
+      expect(@book.category).to eq :category
+    end
+  end
+end
